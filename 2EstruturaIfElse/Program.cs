60e2a0d6b809e1f8068718989bbb10b6feb11ce7
JustinoLucas <justinolucas@hotmail.com>
2024-09-20 15:57:28 -0300

    Estrutura If Else

---
+++ 2EstruturaIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("-- If Else --\n");
+
+Console.WriteLine("Informe a nota do aluno: \t");
+var nota = Convert.ToInt32(Console.ReadLine());
+
+if (nota > 5)
+{
+    Console.WriteLine("Aluno Aprovado");
+}
+else
+{
+    Console.WriteLine("Aluno Reprovado");
+}
+
+Console.WriteLine("\nFim do Processamento...");
